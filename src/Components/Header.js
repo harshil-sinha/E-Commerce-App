06eb72035420c15e8d5cd87822d7f76535bd29dc
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-23 10:21:18 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ const Header = () =>{
         <>
         <div className="card text-center head">
         <div className="card-footer text-body-secondary p-5">
-            <h1>The Generics</h1>
+            <h1 className="text-white">The Generics</h1>
         </div>
         </div>
         </>
