b0b271447857d03aa253e4b3001b4224641dd8eb
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-23 10:17:05 +0530

    Add files via upload

---
+++ src/Components/Button.js
@@ -0,0 +1,11 @@
+const Button = () =>{
+    return (
+        <>
+        <div className="text-center">
+            <a href="#" target="_blank">
+            <button type="button" className="btn btn-primary mt-5 mb-5 p-3">See The Cart</button></a>
+        </div>
+        </>
+    )
+}
+export default Button;
