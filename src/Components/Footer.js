5acb5e806ea473798b029e8d7592fefce7fe2763
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2023-08-23 10:15:44 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,20 @@
+import "../Components/App.css";
 const Footer = () =>{
     return (
         <>
-        <div className="card text-center  mt-2">
-        <div className="card-footer text-body-secondary">
-            <h2>&#169; The Generics</h2>
+        <div className="container-fluid text-center mt-2 bg-info p-4">
+        <div className="row">
+            <div className="col-md-7">
+            <h2 className="text-white">&#169; The Generics</h2>
+            </div>
+            <div className="col-md-4">
+            <h2>
+            <a href="https://www.youtube.com/" class="fa fa-youtube foot" target="_blank"></a>
+            <a href="https://open.spotify.com/" className="fa fa-rss foot" target="_blank"></a>
+            <a href="https://www.facebook.com/profile.php?id=100004529718808" className="fa fa-facebook foot" target="_blank"></a>
+            </h2>
+            </div>
+            
         </div>
         </div>
         </>
